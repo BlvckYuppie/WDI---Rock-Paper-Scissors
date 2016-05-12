dba60bd722f9bc3bb8c9e802631f6a61e118d5b6
Corduroy <Corduroy@192.168.1.60>
2016-05-12 16:08:23 -0400

    Added js alerts, game no longer bound to console log. Modified game verbiage

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,11 +1,11 @@
-////////////////////////////////////////////////
+ ////////////////////////////////////////////////
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
 'use strict';
 
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.");
-    return prompt();
+    console.log("Please choose either 'rock', 'paper', or 'scissors'");
+    return prompt("Please choose either Rock, Paper, or Scissors");
 }
 function randomPlay() {
     var randomNumber = Math.random();
@@ -40,7 +40,7 @@ var getWinner = function(playerMove,computerMove) {
     var winner;
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
+    // The rules of sthe game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     if (playerMove=== computerMove) {
         winner = "tie";
 }
@@ -72,7 +72,9 @@ var getWinner = function(playerMove,computerMove) {
 };
 
 var playToFive= function() {
-    console.log("Let's play Rock, Paper, Scissors");
+    console.log("Let's play Rock, Paper, Scissors... In the Matrix");
+    alert("Let's play Rock, Paper, Scissors... In the Matrix.\n \n Press enter to continue");
+    alert("You are Neo, engaging in an epic rock, paper, scissors battle with Agent Smith to decide the fate of the The Matrix, The Machine World, and Humanity. \n\nThe first player to 5 wins. \n\n The fate of reality lies in your hands. Are You Ready?" )
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
@@ -81,27 +83,33 @@ var playToFive= function() {
         var computerMove = getComputerMove();
         var winner = getWinner(playerMove, computerMove);
             if (winner === "tie") {
-                console.log ("Zion isn't yet victoriious, but it has survived to fight another day. You and agent Smith both picked "+playerMove+"!");
+                console.log ("An intense quarell ends in a stalemate. Neo and Agent Smith both picked  "+playerMove+"!");
+                alert("An intense quarell ends in a stalemate. Neo and Agent Smith both picked "+playerMove+"!");   
                     playerWins+=0, computerWins+=0;
     }
             
             else if (winner === "player") {
-                console.log ("Pow! One for humanity! Zion's "+playerMove+" ravaged Agent Smith's "+computerMove+"!");
-                    playerWins +=1;
+                console.log ("An epic victory for humanity! Neo's "+playerMove+" ravaged Agent Smith's "+computerMove+"!");
+                alert ("An epic victory for humanity! Neo's "+playerMove+" ravaged Agent Smith's "+computerMove+"!");    
+                    playerWins ++;
             }                
             
             else if (winner=== "computer"){
-                console.log ("Score 1 for the singularity... The Machine's "+computerMove+" just completley obliterated Zions "+playerMove+"!");
-                    computerWins +=1;
+                console.log ("Score 1 for the singularity... The Machine's "+computerMove+" just completley obliterated Neo's "+playerMove+"!");
+                alert ("Score 1 for the singularity... The Machine's "+computerMove+" just completley obliterated Neo's "+playerMove+"!");    
+                    computerWins ++;
             }
 
                 console.log("The score is currently "+playerWins+" to Neo V. "+computerWins+" for Agent Smith");
+                alert ("The score is currently "+playerWins+" to Neo V. "+computerWins+" for Agent Smith. Please press enter to continue")
     }
             if (playerWins===5) {
             console.log ("After 5 hard fought victories, Neo successfully rebooted the Matrix... All is right with the world.");
+            alert ("After 5 hard fought victories, Neo successfully rebooted the Matrix... All is right with the world.");
         }
-            else if(playerWins ===5)  {
+            else if(computerWins ===5)  {
             console.log ("I killed you, Mr. Anderson. I watched you die... with a certain satisfaction, I might add.");
+            alert ("I killed you, Mr. Anderson. I watched you die... with a certain satisfaction, I might add.");
         }
         return [playerWins, computerWins];
 };
