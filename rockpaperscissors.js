982e14a86bbbd5050df668f492e091a05525a633
Corduroy <Corduroy@192.168.1.60>
2016-05-12 16:05:28 -0400

    redid getWinner / added console log 4 playToFive/fixed broken loop

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,7 +4,7 @@
 'use strict';
 
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
+    console.log("Please choose either 'rock', 'paper', or 'scissors'.");
     return prompt();
 }
 function randomPlay() {
@@ -21,75 +21,89 @@ function randomPlay() {
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
-function getPlayerMove(move) {    
+var getPlayerMove = function(move) {    
 
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
     return move || getInput();
-}
+};
 
-function getComputerMove(move) {
+var getComputerMove = function(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
     return move || randomPlay();
-}
+};
 
-function getWinner(playerMove,computerMove) {
+var getWinner = function(playerMove,computerMove) {
     var winner;
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    
-    if (playerMove === computerMove) { 
-        winner == "tie";
-    } 
-    else if (playerMove === "rock" && computerMove === "scissors") {
-        winner = "player";
-    }
-    else if (playerMove === "scissors" && computerMove === "paper") {
-        winner = "player"; 
-    } 
-    else if (playerMove === "paper" && computerMove === "rock") {
-        winner = "player"; 
-    } 
-    else { 
-        winner ="computer"; 
-    }   
-        return winner;
+    if (playerMove=== computerMove) {
+        winner = "tie";
 }
+    else if (playerMove=== "rock") {
+        if (computerMove === "scissors") {
+            winner = "player";
+        }
+        else{
+            winner= "computer";
+        }
+}
+    else if (playerMove === "paper") {
+        if (computerMove === "rock") {
+            winner = "player";
+        }
+        else{
+            winner = "computer";
+        }
+}
+    else if (playerMove === "scissors") {
+        if (computerMove === "paper") {
+            winner = "player";
+        }
+        else{
+            winner="computer";
+        }
+    }
+        return winner;
+};
 
-function playToFive() {
+var playToFive= function() {
     console.log("Let's play Rock, Paper, Scissors");
     var playerWins = 0;
     var computerWins = 0;
     // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    while (playerWins < 5 && computerWins < 5){
+        while (playerWins < 5 && computerWins <5){
         var playerMove = getPlayerMove();
         var computerMove = getComputerMove();
-        var winner = getWinner();
-            if ( winner = "player") {
-                console.log ("Pow! One for humanity! Your "+playerMove+" ravaged wall-e's "+computerMove+"!");
-                    playerWins +=1;
+        var winner = getWinner(playerMove, computerMove);
+            if (winner === "tie") {
+                console.log ("Zion isn't yet victoriious, but it has survived to fight another day. You and agent Smith both picked "+playerMove+"!");
+                    playerWins+=0, computerWins+=0;
     }
-            else if (winner ="computer") {
+            
+            else if (winner === "player") {
+                console.log ("Pow! One for humanity! Zion's "+playerMove+" ravaged Agent Smith's "+computerMove+"!");
+                    playerWins +=1;
+            }                
+            
+            else if (winner=== "computer"){
                 console.log ("Score 1 for the singularity... The Machine's "+computerMove+" just completley obliterated Zions "+playerMove+"!");
                     computerWins +=1;
-            }                
-            else {
-                console.log ("Zion hasn't won, but it has survived to fight another day. You and agent Smith both picked "+playerMove+"!");
             }
 
-            console.log("The score is currently "+playerWins+" to Neo V. "+computerWins+" for Agent Smith");
+                console.log("The score is currently "+playerWins+" to Neo V. "+computerWins+" for Agent Smith");
     }
-        if (playerWins===5) {
+            if (playerWins===5) {
             console.log ("After 5 hard fought victories, Neo successfully rebooted the Matrix... All is right with the world.");
         }
-        else {
+            else if(playerWins ===5)  {
             console.log ("I killed you, Mr. Anderson. I watched you die... with a certain satisfaction, I might add.");
         }
-    return [playerWins, computerWins];
+        return [playerWins, computerWins];
 };
 
 playToFive();
